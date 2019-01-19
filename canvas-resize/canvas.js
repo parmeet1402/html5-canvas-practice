4d86cdfdc5ca6e99240cf033a987ec743905f9dc
Parmeet <parmeet.asi@gmail.com>
2019-01-19 15:05:05 +0530

    added html5-resize

---
+++ canvas-resize/canvas.js
@@ -0,0 +1,5 @@
+const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d");
+// Alter width and height
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
